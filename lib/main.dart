e97e91d9e355bcf4b25d5f07bce7b1ad8c412340
STG7X <codewars42@gmail.com>
2024-11-10 13:37:50 +0300

    Move CalendarWidget to a separate folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:table_calendar/table_calendar.dart';
+import 'package:habit_tracker/widgets/calendar.dart';
 
 enum Recurrence { daily, weekly, monthly }
 
@@ -246,46 +246,3 @@ class StatisticsScreen extends StatelessWidget {
     );
   }
 }
-
-class CalendarWidget extends StatefulWidget {
-  final Function(DateTime, DateTime) onDaySelected;
-
-  CalendarWidget({Key? key, required this.onDaySelected}) : super(key: key);
-
-  @override
-  _CalendarWidgetState createState() => _CalendarWidgetState();
-}
-
-class _CalendarWidgetState extends State<CalendarWidget> {
-  DateTime _focusedDay = DateTime.now();
-
-  @override
-  Widget build(BuildContext context) {
-    final state = context.findAncestorStateOfType<_HabitListScreenState>()!;
-
-    return TableCalendar(
-      focusedDay: _focusedDay,
-      firstDay: DateTime(2000),
-      lastDay: DateTime(2100),
-      onDaySelected: (selectedDay, focusedDay) {
-        setState(() {
-          state._selectedDate = selectedDay;
-          _focusedDay = focusedDay;
-        });
-        widget.onDaySelected(selectedDay, focusedDay);
-      },
-      calendarStyle: CalendarStyle(
-        selectedDecoration: BoxDecoration(
-          color: Colors.teal,
-          shape: BoxShape.circle,
-        ),
-        selectedTextStyle: TextStyle(color: Colors.white),
-        defaultDecoration: BoxDecoration(shape: BoxShape.rectangle),
-      ),
-      selectedDayPredicate: (day) {
-        return day.isSameDate(state._selectedDate);
-      },
-    );
-  }
-}
-
---
+++ lib/widgets/calendar.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:habit_tracker/main.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+class CalendarWidget extends StatefulWidget {
+  final Function(DateTime, DateTime) onDaySelected;
+  CalendarWidget({Key? key, required this.onDaySelected}) : super(key: key);
+  @override
+  _CalendarWidgetState createState() => _CalendarWidgetState();
+}
+class _CalendarWidgetState extends State<CalendarWidget> {
+  DateTime _focusedDay = DateTime.now();
+  DateTime _selectedDate = DateTime.now();  // Seçilen tarihi burada tutuyoruz.
+  @override
+  Widget build(BuildContext context) {
+    return TableCalendar(
+      focusedDay: _focusedDay,
+      firstDay: DateTime(2000),
+      lastDay: DateTime(2100),
+      onDaySelected: (selectedDay, focusedDay) {
+        setState(() {
+          _selectedDate = selectedDay;  // Seçilen tarihi güncelliyoruz.
+          _focusedDay = focusedDay;  // Focused günü güncelliyoruz.
+        });
+        widget.onDaySelected(selectedDay, focusedDay);  // Ana widget'tan gelen onDaySelected fonksiyonunu çağırıyoruz
+      },
+      calendarStyle: CalendarStyle(
+        selectedDecoration: BoxDecoration(
+          color: Colors.teal,
+          shape: BoxShape.circle,
+        ),
+        selectedTextStyle: TextStyle(color: Colors.white),
+        defaultDecoration: BoxDecoration(shape: BoxShape.rectangle),
+      ),
+      selectedDayPredicate: (day) {
+        return day.isSameDate(_selectedDate);  // Seçilen tarihi kontrol ediyoruz.
+      },
+    );
+  }
+}
